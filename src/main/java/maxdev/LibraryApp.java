eff8f1ae2aec1d95d4626e15e1c47dcc88c9ce4d
mspizhovyi <max.spizhovyi@myokanagan.bc.ca>
2025-12-01 01:24:44 -0800

    Uploaded sequence diagrams

--- src/main/java/maxdev/LibraryApp.java
+++ src/main/java/maxdev/LibraryApp.java
@@ -6,7 +6,6 @@ public class LibraryApp {
 	private static final LibrarianController librarian = new LibrarianController();
 
 	public static void main(String[] args) {
-
 		librarian.addPaperBook("Dune");
 		librarian.addMember("Alice");
 
